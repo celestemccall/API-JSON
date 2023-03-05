c8e3f8378b1b63291a484c32cc9f4ce027b7a9a8
Celeste McCall <celestemccall93@gmail.com>
2023-03-05 01:40:59 -0600

    Initial Commit

---
+++ Ron SwansonAndKanyeWest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace RonSwansonAndKanyeWest
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kanye: I am going to tell one of my famous quotes, then you should tell one of yours!");
+            Console.WriteLine("Ron: Sounds good to me, lets do it!");
+
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGenerator.kanyeQuote();
+
+                QuoteGenerator.ronQuote();
+            }
+
+        }
+    }
+}
